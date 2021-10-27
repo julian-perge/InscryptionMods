b32115bf369afd7b70f73e0e90eabf526d978d9a
julian-perge <julianperge@gmail.com>
2021-10-27 14:19:52 -0400

    first commit

---
+++ StatBoostSequencerPatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+using DiskCardGame;
+using HarmonyLib;
+using PeterHan.PLib.Utils;
+
+namespace InscryptionModsFramework
+{
+	// DiskCardGame.StatBoostSequencerPatch+<StatBoostSequence>d__12 <-- THIS IS THE ONE
+
+	[HarmonyPatch]
+	class StatBoostSequencerPatch
+	{
+
+		[HarmonyTargetMethods]
+		static IEnumerable<MethodBase> ReturnMoveNextMethodFromNestedEnumerator(Harmony _)
+		{
+			Type targetType = AccessTools.TypeByName("DiskCardGame.StatBoostSequencerPatch+<StatBoostSequence>d__12");
+			return AccessTools.GetDeclaredMethods(targetType).Where(m => m.Name.Equals("MoveNext"));
+		}
+
+		[HarmonyTranspiler]
+		internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			return PPatchTools.ReplaceConstant(instructions, 0.225f, 0f, false);
+		}
+
+
+	}
+
+}
