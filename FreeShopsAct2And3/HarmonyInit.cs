af77a22c81582c67d1e7cc168ba44fb8c5678854
julian-perge <julianperge@gmail.com>
2021-11-13 22:04:37 -0500

    Removed some depends in PeltsAreAlwaysFree Renamed

--- FreeShopsAct2And3/HarmonyInit.cs
+++ FreeShopsAct2And3/HarmonyInit.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using BepInEx;
+﻿using BepInEx;
 using HarmonyLib;
 
-namespace StartWithMaxCurrency
+namespace FreeShopsAct2And3
 {
 	[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
 	public class HarmonyInit : BaseUnityPlugin
 	{
-		
 		private const string PluginGuid = "com.julianperge";
 		private const string PluginName = "startWithMaxCurrency";
 		private const string PluginVersion = "1.0";
-		
+
 		void Awake()
 		{
 			var harmony = new Harmony(PluginGuid);
