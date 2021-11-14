6eee58a73af3988aeaff4317f02a22d2b7319fab
julian-perge <julianperge@gmail.com>
2021-11-13 22:02:17 -0500

    Added cards for PorcelainMask and TickleMonster

---
+++ AddAllSCP/SCP_035_Porcelain_Mask/Card.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using APIPlugin;
+using CardLoaderPlugin.lib;
+using DiskCardGame;
+using UnityEngine;
+
+namespace AddAllSCP.SCP_035_Porcelain_Mask
+{
+	public static class Card
+	{
+		public static void InitCard()
+		{
+			List<CardMetaCategory> metaCategories = CardUtils.getNormalCardMetadata;
+			List<Ability> abilities = new List<Ability> { MaskAbility.ability, Ability.PreventAttack };
+
+			Texture2D defaultTexture =
+				CardUtils.getAndloadImageAsTexture("BepInEx/plugins/CardLoader/Artwork/scp_035_small.png");
+
+			string name = "SCP_035_PorcelainMask";
+			string displayName = "Porcelain Mask";
+			string desc = "A highly corrosive and degenerative viscous liquid constantly seeps from the eye and mouth holes.";
+
+			NewCard.AddToPool(name, displayName, 0, 1,
+				metaCategories, CardComplexity.Simple, CardTemple.Nature, 
+				bloodCost: 1, defaultTexture: defaultTexture, abilities: abilities
+			);
+		}
+	}
+}
---
+++ AddAllSCP/SCP_999_Tickle_Monster/Card.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using APIPlugin;
+using CardLoaderPlugin.lib;
+using DiskCardGame;
+using UnityEngine;
+
+namespace AddAllSCP.SCP_999_Tickle_Monster
+{
+	public static class Card
+	{
+		
+		public static void InitCard()
+		{
+			List<CardMetaCategory> metaCategories = CardUtils.getNormalCardMetadata;
+
+			List<Ability> abilities = new List<Ability> { Ability.DebuffEnemy, Ability.BuffNeighbours };
+
+			byte[] defaultTextureBytes =
+				System.IO.File.ReadAllBytes("BepInEx/plugins/CardLoader/Artwork/scp_999_small.png");
+			Texture2D defaultTexture = new Texture2D(2, 2);
+			defaultTexture.LoadImage(defaultTextureBytes);
+			
+			NewCard.Add("SCP_999_TickleMonster", metaCategories, CardComplexity.Simple, CardTemple.Nature, "Tickle Monster",
+				0, 2,
+				"Simply touching SCP-999’s surface causes an immediate euphoria, which intensifies the longer one is exposed to SCP-999, and lasts long after separation from the creature",
+				cost: 1, tex: defaultTexture, abilities: abilities);
+		}
+		
+	}
+	
+}
