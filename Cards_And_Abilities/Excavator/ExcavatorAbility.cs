fdf7ff2a8c2a0d21b2c41eda39f84202b86ef0ce
julian-perge <julianperge@gmail.com>
2021-11-19 16:21:44 -0500

    Added Excavator ability

---
+++ Cards_And_Abilities/Excavator/ExcavatorAbility.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Linq;
+using DiskCardGame;
+using UnityEngine;
+
+namespace Excavator
+{
+	public class ExcavatorAbility : AbilityBehaviour
+	{
+		public static Ability ability;
+		public override Ability Ability { get { return ability; } }
+
+		public override bool RespondsToResolveOnBoard()
+		{
+			return true;
+		}
+
+		public override IEnumerator OnResolveOnBoard()
+		{
+			yield return base.PreSuccessfulTriggerSequence();
+
+			// only get a list of slots that are not null and have cards in them
+			var slotsWithCards = Singleton<BoardManager>
+				.Instance
+				.GetSlots(true)
+				.Where(slot => slot && slot.Card);
+
+			Singleton<ViewManager>.Instance.SwitchToView(View.Board, false, false);
+			// now only check those filtered cards that have terrain traits
+			foreach (var slot in slotsWithCards.Where(slot => slot.Card.Info.HasTrait(Trait.Terrain)))
+			{
+				yield return new WaitForSeconds(0.1f);
+				yield return slot.Card.Die(false, base.Card);
+				// this is copied from BeesOnHit Ability
+				if (Singleton<ViewManager>.Instance.CurrentView != View.Default)
+				{
+					yield return new WaitForSeconds(0.2f);
+					Singleton<ViewManager>.Instance.SwitchToView(View.Default, false, false);
+					yield return new WaitForSeconds(0.2f);
+				}
+
+				// Spawn squirrel in your hand
+				yield return Singleton<CardSpawner>.Instance.SpawnCardToHand(CardLoader.GetCardByName("Squirrel"), null);
+			}
+
+			yield return base.LearnAbility(0.25f);
+			yield break;
+		}
+	}
+}
---
+++ Cards_And_Abilities/Excavator/HarmonyInit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Logging;
+using DiskCardGame;
+using HarmonyLib;
+
+namespace Excavator
+{
+	[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+	[BepInDependency("cyantist.inscryption.api", BepInDependency.DependencyFlags.HardDependency)]
+	public class HarmonyInit : BaseUnityPlugin
+	{
+		public const string PluginGuid = "julian.inscryption.abilities.excavator";
+		private const string PluginName = "Custom_Ability_Excavator";
+		private const string PluginVersion = "1.1";
+
+		internal static ManualLogSource Log;
+
+		void Awake()
+		{
+			Log = base.Logger;
+
+			NewAbility ability = NestAbility.InitAbility();
+			var abilities = new List<Ability>() { ability.ability };
+			new CustomCard("Stinkbug_Talking") { abilities = abilities };
+
+			var harmony = new Harmony(PluginGuid);
+			harmony.PatchAll();
+		}
+	}
+}
