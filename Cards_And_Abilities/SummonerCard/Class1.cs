3f537211ce4745ffda4509721316e5d48d47f7eb
julian-perge <julianperge@gmail.com>
2021-11-23 20:27:45 -0500

    Moved API to lib folder, updates to a few classes. Added SigilADay.dll

--- Cards_And_Abilities/SummonerCard/Class1.cs
+++ Cards_And_Abilities/SummonerCard/Class1.cs
@@ -1,6 +1,26 @@
-﻿namespace SummonerCard
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace SummonerCard
 {
-	public class Class1
+	[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+	[BepInDependency("cyantist.inscryption.api", BepInDependency.DependencyFlags.HardDependency)]
+	[BepInDependency("julianperge.inscryption.sigiladay", BepInDependency.DependencyFlags.HardDependency)]
+	public class HarmonyInit : BaseUnityPlugin
 	{
+		public const string PluginGuid = "julian.inscryption.cards.testing";
+		private const string PluginName = "TestingDeck";
+		private const string PluginVersion = "1.0.0";
+
+		internal static ManualLogSource Log;
+
+		void Awake()
+		{
+			Log = base.Logger;
+
+			var harmony = new Harmony(PluginGuid);
+			harmony.PatchAll();
+		}
 	}
 }
