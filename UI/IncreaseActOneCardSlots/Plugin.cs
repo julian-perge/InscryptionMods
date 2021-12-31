03542b0da641775723ef20493ebd1c5792ee9057
julian-perge <julianperge@gmail.com>
2021-12-31 11:37:35 -0500

    Version 1.1.0

--- UI/IncreaseActOneCardSlots/Plugin.cs
+++ UI/IncreaseActOneCardSlots/Plugin.cs
@@ -1,24 +1,20 @@
-﻿using BepInEx;
-using BepInEx.Logging;
-using HarmonyLib;
-
-namespace IncreaseActOneCardSlots
+﻿namespace IncreaseActOneCardSlots
 {
-    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
-    public class Plugin : BaseUnityPlugin
-    {
-        public const string PluginGuid = "julianperge.inscryption.act1.increaseCardSlots";
-        public const string PluginName = "IncreaseActOneCardSlots";
-        private const string PluginVersion = "1.0.0";
+	[BepInEx.BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+	public class Plugin : BepInEx.BaseUnityPlugin
+	{
+		public const string PluginGuid = "julianperge.inscryption.act1.increaseCardSlots";
+		public const string PluginName = "IncreaseActOneCardSlots";
+		private const string PluginVersion = "1.1.0";
 
-        internal static ManualLogSource Log;
+		internal static BepInEx.Logging.ManualLogSource Log;
 
-        private void Awake()
-        {
-            Log = base.Logger;
+		private void Awake()
+		{
+			Log = base.Logger;
 
-            var harmony = new Harmony(PluginGuid);
-            harmony.PatchAll();
-        }
-    }
+			var harmony = new HarmonyLib.Harmony(PluginGuid);
+			harmony.PatchAll();
+		}
+	}
 }
