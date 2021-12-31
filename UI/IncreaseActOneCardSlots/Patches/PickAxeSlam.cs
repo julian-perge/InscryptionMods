bd28996681e3bab55f9b16c08d964e77997ddeb4
julian-perge <julianperge@gmail.com>
2021-12-31 10:11:04 -0500

    Corrected references for PickAxeSlam.cs

--- UI/IncreaseActOneCardSlots/Patches/PickAxeSlam.cs
+++ UI/IncreaseActOneCardSlots/Patches/PickAxeSlam.cs
@@ -1,39 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
-using HarmonyLib;
+﻿using Enumerable = System.Linq.Enumerable;
 
 namespace IncreaseActOneCardSlots.Patches
 {
-	[HarmonyPatch]
+	[HarmonyLib.HarmonyPatch]
 	public class PickAxeSlam
 	{
-		[HarmonyTargetMethods]
-		static IEnumerable<MethodBase> ReturnMoveNextMethodFromNestedEnumerator(Harmony _)
+		[HarmonyLib.HarmonyTargetMethods]
+		static System.Collections.Generic.IEnumerable<System.Reflection.MethodBase>
+			ReturnMoveNextMethodFromNestedEnumerator(HarmonyLib.Harmony _)
 		{
 			// StrikeCardSlot is the IEnumerator method, but there's a hidden compiler class, <StrikeCardSlot>d__2,
 			//	that actually has all the byte code to look for.
-			Type getEnumeratorType = AccessTools.TypeByName("DiskCardGame.PickAxeSlam+<StrikeCardSlot>d__2");
-			return AccessTools.GetDeclaredMethods(getEnumeratorType).Where(m => m.Name.Equals("MoveNext"));
+			System.Type getEnumeratorType =
+				HarmonyLib.AccessTools.TypeByName("DiskCardGame.PickAxeSlam+<StrikeCardSlot>d__2");
+			return Enumerable.Where(HarmonyLib.AccessTools.GetDeclaredMethods(getEnumeratorType),
+				m => m.Name.Equals("MoveNext"));
 		}
 
-		[HarmonyTranspiler]
-		internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		[HarmonyLib.HarmonyTranspiler]
+		internal static System.Collections.Generic.IEnumerable<HarmonyLib.CodeInstruction> Transpiler(
+			System.Collections.Generic.IEnumerable<HarmonyLib.CodeInstruction> instructions)
 		{
-			return new CodeMatcher(instructions)
+			return new HarmonyLib.CodeMatcher(instructions)
 				.Start()
 				// this is the OpCode before the integer for 3 is used
 				// the reason we go to this one is that there is another ldc_i4_3 later in the instructions that we do not want to modify
 				.MatchForward(true,
-					new CodeMatch(OpCodes.Ldloca_S),
-					new CodeMatch(OpCodes.Ldc_I4_3)
+					new HarmonyLib.CodeMatch(System.Reflection.Emit.OpCodes.Ldloca_S),
+					new HarmonyLib.CodeMatch(System.Reflection.Emit.OpCodes.Ldc_I4_3)
 				)
 				// set the 3 to a 4
-				.Set(OpCodes.Ldc_I4_4, null)
+				.Set(System.Reflection.Emit.OpCodes.Ldc_I4_4, null)
 				// now look for the -1.4 float number to change where the pick axe strikes the center of the card
-				.MatchForward(false, new CodeMatch(OpCodes.Ldc_R4, -1.4f))
+				.MatchForward(false, new HarmonyLib.CodeMatch(System.Reflection.Emit.OpCodes.Ldc_R4, -1.4f))
 				// change to -1.275
 				.SetOperandAndAdvance(-1.275f)
 				.InstructionEnumeration();
