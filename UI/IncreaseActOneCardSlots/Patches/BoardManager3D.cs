6068f9e31bc789e46204abb54f4d8faa30d3f748
julian-perge <julianperge@gmail.com>
2021-12-31 09:45:08 -0500

    Removed GiantAnimatedPortraitPatch.cs as that logic now exists in ModifyLocalPositionsOfTableObjects.cs

---
+++ UI/IncreaseActOneCardSlots/Patches/BoardManager3D.cs
@@ -0,0 +1,98 @@
+﻿namespace IncreaseActOneCardSlots
+{
+	[HarmonyLib.HarmonyPatch(typeof(DiskCardGame.BoardManager3D))]
+	public class BoardManager3DPatch
+	{
+		private static readonly DiskCardGame.CardSlot CardSlotPrefab =
+			ResourceBank.Get<DiskCardGame.CardSlot>("Prefabs/Cards/CardSlot");
+
+		private static readonly DiskCardGame.HighlightedInteractable OpponentQueueSlotPrefab =
+			ResourceBank.Get<DiskCardGame.HighlightedInteractable>("Prefabs/Cards/QueueSlot");
+
+		[HarmonyLib.HarmonyPrefix, HarmonyLib.HarmonyPatch(nameof(DiskCardGame.BoardManager3D.Initialize))]
+		public static void ModifyPositionsOfItemsPatch(DiskCardGame.BoardManager3D __instance)
+		{
+			UnityEngine.Transform boardObj = UnityEngine.GameObject.Find("CardBattle").transform.Find("Board");
+			UnityEngine.Transform playerSlots = boardObj.Find("PlayerSlots");
+			if (playerSlots.childCount == 4)
+			{
+				/// moving items so that they don't overlap on the board
+				Plugin.Log.LogDebug($"Setting new Items position");
+				UnityEngine.GameObject.Find("Items").transform.localPosition = new UnityEngine.Vector3(5.2f, 5.01f, 0f);
+
+				ModifyBoardChildrenPositions(boardObj);
+
+				/// creating new slots
+				// player
+				DiskCardGame.CardSlot playerSlot5 = CreateSlot<DiskCardGame.CardSlot>(
+					"PlayerCardSlot5", playerSlots.transform, new UnityEngine.Vector3(3.4f, 0f, 0f), CardSlotPrefab
+				);
+
+				// opponent
+				UnityEngine.Transform oppSlots = boardObj.Find("OpponentSlots");
+				DiskCardGame.CardSlot opponentSlot5 = CreateSlot<DiskCardGame.CardSlot>(
+					"OpponentCardSlot5", oppSlots.transform, new UnityEngine.Vector3(3.4f, 0f, 0f), CardSlotPrefab
+				);
+				UnityEngine.Transform oppCardSlot5Quad = opponentSlot5.transform.Find("Quad");
+				// rotate slot on y-axis so that the paw image faces the Player like the other slots
+				oppCardSlot5Quad.rotation = UnityEngine.Quaternion.Euler(90f, 180f, 0f);
+
+				// opponent queue
+				DiskCardGame.HighlightedInteractable opponentQueueSlot5 = CreateSlot<DiskCardGame.HighlightedInteractable>(
+					"OpponentQueueSlot5",
+					oppSlots.transform,
+					new UnityEngine.Vector3(3.4f, 0f, 2.01f),
+					OpponentQueueSlotPrefab
+				);
+
+				/// opposing slots
+				playerSlot5.opposingSlot = opponentSlot5;
+				opponentSlot5.opposingSlot = playerSlot5;
+
+				/// now add each slot to the respective list
+				AddSlotsToBoardInstance(__instance, playerSlot5, opponentSlot5, opponentQueueSlot5);
+			}
+		}
+
+		private static void AddSlotsToBoardInstance(DiskCardGame.BoardManager3D __instance,
+			DiskCardGame.CardSlot playerSlot5, DiskCardGame.CardSlot opponentSlot5,
+			DiskCardGame.HighlightedInteractable opponentQueueSlot5)
+		{
+			Plugin.Log.LogDebug($"Adding player slot 5 to BoardManager3D field");
+			__instance.playerSlots.Add(playerSlot5);
+
+			Plugin.Log.LogDebug($"Adding opponent slot 5 to BoardManager3D field");
+			__instance.opponentSlots.Add(opponentSlot5);
+
+			Plugin.Log.LogDebug($"Adding opponent queue slot 5 to BoardManager3D field");
+			__instance.opponentQueueSlots.Add(opponentQueueSlot5);
+		}
+
+		private static void ModifyBoardChildrenPositions(UnityEngine.Transform boardObj)
+		{
+			// board
+			Plugin.Log.LogDebug($"Setting new Board position");
+			boardObj.localPosition = new UnityEngine.Vector3(0f, 5f, 0f);
+
+			// card draw piles
+			Plugin.Log.LogDebug($"Setting new CardDrawPiles position");
+			boardObj.Find("CardDrawPiles").transform.localPosition = new UnityEngine.Vector3(0.75f, 0f, 0f);
+
+			// sacrifice tokens
+			Plugin.Log.LogDebug($"Setting new SacrificeTokens position");
+			boardObj.Find("SacrificeTokens").transform.localPosition = new UnityEngine.Vector3(4.2f, 0.03f, -0.4f);
+		}
+
+		private static T CreateSlot<T>(
+			string name, UnityEngine.Transform parent, UnityEngine.Vector3 newPosition,
+			DiskCardGame.HighlightedInteractable prefab)
+			where T : DiskCardGame.HighlightedInteractable
+		{
+			Plugin.Log.LogDebug($"Creating slot {name}");
+			T slot = (T)UnityEngine.Object.Instantiate(prefab, parent);
+			slot.name = name;
+			slot.transform.localPosition = newPosition;
+			return slot;
+		}
+	}
+}
---
+++ UI/IncreaseActOneCardSlots/Patches/ModifyLocalPositionsOfTableObjects.cs
@@ -0,0 +1,105 @@
+﻿namespace IncreaseActOneCardSlots.Patches
+{
+	public class ModifyLocalPositionsOfTableObjects
+	{
+		// KnivesTableEffects(Clone)/RightSide/RepeatingConveyorKnives, change z-axis to 4.5
+
+		[HarmonyLib.HarmonyPatch(typeof(DiskCardGame.Part1BossOpponent),
+			nameof(DiskCardGame.Part1BossOpponent.SpawnScenery))]
+		public class ModifyTreePositionsForProspector
+		{
+			private static readonly System.Collections.Generic.List<int> TreesToModifyPositiveZedPosition =
+				new() { 2, 3, 4, 5 };
+
+			static void SetNewPosForTree(UnityEngine.Transform transform, UnityEngine.Vector3 newPosition)
+			{
+				// group47
+				transform.GetChild(0).localPosition = newPosition;
+				// tree
+				transform.GetChild(1).localPosition = newPosition;
+			}
+
+			[HarmonyLib.HarmonyPostfix]
+			static void Postfix(DiskCardGame.Part1BossOpponent __instance)
+			{
+				var sceneryObj = __instance.sceneryObject;
+
+				if (__instance is DiskCardGame.ProspectorBossOpponent)
+				{
+					Plugin.Log.LogDebug(
+						$"[{__instance.GetType()}] ForestTableEffects has been spawned, scene object is [{sceneryObj}]");
+
+					TreesToModifyPositiveZedPosition.ForEach(treeIndex =>
+					{
+						// ForestTableEffects -> GetChild(1) == SurroundingTrees
+						UnityEngine.Vector3 newPos = new UnityEngine.Vector3(0f, 0f, treeIndex == 4 ? -2f : 2f);
+						SetNewPosForTree(sceneryObj.transform.GetChild(1).GetChild(treeIndex), newPos);
+
+						Plugin.Log.LogDebug($"moving tree {treeIndex} to new position [{newPos.ToString()}]");
+					});
+				}
+				else if (__instance is DiskCardGame.TrapperTraderBossOpponent)
+				{
+					Plugin.Log.LogDebug(
+						$"[{__instance.GetType()}] KnivesTableEffects has been spawned, scene object is [{sceneryObj}]");
+
+					// KnivesTableEffects -> GetChild(0) == LeftSide
+					UnityEngine.Transform knivesLeftSideTransform = sceneryObj.transform.GetChild(0);
+					UnityEngine.Vector3 knivesLeftSideLocalPos = knivesLeftSideTransform.transform.localPosition;
+					Plugin.Log.LogDebug($"moving left side knives back a little...");
+					knivesLeftSideTransform.localPosition = new UnityEngine.Vector3(
+						-1f, knivesLeftSideLocalPos.y, knivesLeftSideLocalPos.z
+					);
+
+					// KnivesTableEffects -> GetChild(1) == RightSide
+					UnityEngine.Transform knivesRightSideTransform = sceneryObj.transform.GetChild(1);
+					UnityEngine.Vector3 knivesRightSideLocalPos = knivesRightSideTransform.transform.localPosition;
+					Plugin.Log.LogDebug($"moving right side knives back a little...");
+					knivesRightSideTransform.localPosition = new UnityEngine.Vector3(
+						1, knivesRightSideLocalPos.y, knivesRightSideLocalPos.z
+					);
+				}
+			}
+		}
+
+		[HarmonyLib.HarmonyPostfix,
+		 HarmonyLib.HarmonyPatch(nameof(DiskCardGame.BoardManager3D.TransitionAndResolveCreatedCard))]
+		public static void ChangeScaleOfMoonCardToFitAcrossAllSlots(
+			DiskCardGame.PlayableCard card, DiskCardGame.CardSlot slot, float transitionLength, bool resolveTriggers = true
+		)
+		{
+			if (card.Info.HasTrait(DiskCardGame.Trait.Giant))
+			{
+				Plugin.Log.LogDebug($"Setting new scaling and position of the moon");
+				// Card -> Quad -> CardBase
+				var cardBase = card.transform.GetChild(0).GetChild(0);
+
+				var localScale = cardBase.localScale;
+				cardBase.localScale = new UnityEngine.Vector3(5.75f, localScale.y, localScale.z);
+
+				var localPosition = cardBase.localPosition;
+				cardBase.localPosition = new UnityEngine.Vector3(-2.25f, localPosition.y, localPosition.z);
+			}
+		}
+
+		[HarmonyLib.HarmonyPatch]
+		public class SetCandleAndRuleBookPositionsPatch
+		{
+			[HarmonyLib.HarmonyPrefix,
+			 HarmonyLib.HarmonyPatch(typeof(DiskCardGame.TableRuleBook), nameof(DiskCardGame.TableRuleBook.Awake))]
+			public static void ChangeTableRuleBookDefaultPosition(DiskCardGame.TableRuleBook __instance)
+			{
+				Plugin.Log.LogDebug($"Setting new position for TableRuleBook");
+				__instance.transform.localPosition = new UnityEngine.Vector3(-4.69f, 0f, -4f);
+			}
+
+			[HarmonyLib.HarmonyPrefix,
+			 HarmonyLib.HarmonyPatch(typeof(DiskCardGame.CandleHolder), nameof(DiskCardGame.CandleHolder.Awake))]
+			public static void ChangeCandleHolderLocalPosition(DiskCardGame.CandleHolder __instance)
+			{
+				Plugin.Log.LogDebug($"Setting new position for CandleHolder");
+				__instance.transform.localPosition = new UnityEngine.Vector3(6f, -0.006000042f, 1.1f);
+			}
+		}
+	}
+}
