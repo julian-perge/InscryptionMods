adfef9133e31d1c3ce8bebf3098ca844e727f961
julian-perge <julianperge@gmail.com>
2021-12-31 08:57:51 -0500

    Refactored predicate for changing magic number from 3 to 4. Added logic for modifying float magic number -1.4 to -1.275 to change where the pick axe strikes the slot position.

--- UI/IncreaseActOneCardSlots/Patches/PickAxeSlam.cs
+++ UI/IncreaseActOneCardSlots/Patches/PickAxeSlam.cs
@@ -8,7 +8,7 @@ using HarmonyLib;
 namespace IncreaseActOneCardSlots.Patches
 {
 	[HarmonyPatch]
-	public class PickAxeSlamPatch
+	public class PickAxeSlam
 	{
 		[HarmonyTargetMethods]
 		static IEnumerable<MethodBase> ReturnMoveNextMethodFromNestedEnumerator(Harmony _)
@@ -26,11 +26,16 @@ namespace IncreaseActOneCardSlots.Patches
 				.Start()
 				// this is the OpCode before the integer for 3 is used
 				// the reason we go to this one is that there is another ldc_i4_3 later in the instructions that we do not want to modify
-				.MatchForward(false, new CodeMatch(OpCodes.Ldloca_S))
-				// go to the next instruction, which is the ldc_i4_3 OpCode
-				.Advance(1)
+				.MatchForward(true,
+					new CodeMatch(OpCodes.Ldloca_S),
+					new CodeMatch(OpCodes.Ldc_I4_3)
+				)
 				// set the 3 to a 4
 				.Set(OpCodes.Ldc_I4_4, null)
+				// now look for the -1.4 float number to change where the pick axe strikes the center of the card
+				.MatchForward(false, new CodeMatch(OpCodes.Ldc_R4, -1.4f))
+				// change to -1.275
+				.SetOperandAndAdvance(-1.275f)
 				.InstructionEnumeration();
 		}
 	}
