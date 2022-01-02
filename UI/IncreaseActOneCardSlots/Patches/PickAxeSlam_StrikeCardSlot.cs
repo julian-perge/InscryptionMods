60403572cefa727d0708c9df69d096fcf0825e90
julian-perge <julianperge@gmail.com>
2022-01-02 15:04:58 -0500

    Updated PickAxeSlam patch to now work with Kaycee's mod

--- UI/IncreaseActOneCardSlots/Patches/PickAxeSlam_StrikeCardSlot.cs
+++ UI/IncreaseActOneCardSlots/Patches/PickAxeSlam_StrikeCardSlot.cs
@@ -6,7 +6,7 @@ using HarmonyLib;
 namespace IncreaseActOneCardSlots.Patches
 {
 	[HarmonyPatch]
-	public class PickAxeSlam
+	public class PickAxeSlam_StrikeCardSlot
 	{
 		[HarmonyTargetMethods]
 		static IEnumerable<System.Reflection.MethodBase> ReturnMoveNextMethodFromNestedEnumerator(Harmony _)
@@ -14,7 +14,7 @@ namespace IncreaseActOneCardSlots.Patches
 			// StrikeCardSlot is the IEnumerator method, but there's a hidden compiler class, <StrikeCardSlot>d__2,
 			//	that actually has all the byte code to look for.
 			System.Type getEnumeratorType = AccessTools.TypeByName("DiskCardGame.PickAxeSlam+<StrikeCardSlot>d__2");
-			return Enumerable.Where(AccessTools.GetDeclaredMethods(getEnumeratorType), m => m.Name.Equals("MoveNext"));
+			return AccessTools.GetDeclaredMethods(getEnumeratorType).Where(m => m.Name == ("MoveNext"));
 		}
 
 		[HarmonyTranspiler]
@@ -24,12 +24,11 @@ namespace IncreaseActOneCardSlots.Patches
 				.Start()
 				// this is the OpCode before the integer for 3 is used
 				// the reason we go to this one is that there is another ldc_i4_3 later in the instructions that we do not want to modify
-				.MatchForward(true,
-					new CodeMatch(OpCodes.Ldloca_S),
+				.MatchForward(false,
 					new CodeMatch(OpCodes.Ldc_I4_3)
 				)
 				// set the 3 to a 4
-				.Set(OpCodes.Ldc_I4_4, null)
+				.SetOpcodeAndAdvance(OpCodes.Ldc_I4_4)
 				// now look for the -1.4 float number to change where the pick axe strikes the center of the card
 				.MatchForward(false, new CodeMatch(OpCodes.Ldc_R4, -1.4f))
 				// change to -1.275
