d9b8cdfd72d3f76056cfe21a714848afe3e67014
julian-perge <julianperge@gmail.com>
2022-01-02 15:46:42 -0500

    Renamed HarmonyInit to Plugin for consistency. General formatting.

--- Events/FirePitAlwaysAbleToUpgrade/Plugin.cs
+++ Events/FirePitAlwaysAbleToUpgrade/Plugin.cs
@@ -4,14 +4,18 @@ using HarmonyLib;
 namespace FirePitAlwaysAbleToUpgrade
 {
 	[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
-	public class HarmonyInit : BaseUnityPlugin
+	public class Plugin : BaseUnityPlugin
 	{
 		private const string PluginGuid = "julianperge.inscryption.act1.firePitAlwaysAbleToUpgrade";
 		private const string PluginName = "FirePitAlwaysAbleToUpgrade";
 		private const string PluginVersion = "1.3.0";
 
+		internal static BepInEx.Logging.ManualLogSource Log;
+
 		void Awake()
 		{
+			Log = base.Logger;
+
 			var harmony = new Harmony(PluginGuid);
 			harmony.PatchAll();
 		}
