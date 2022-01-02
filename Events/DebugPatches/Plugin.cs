5bb7879c0257f20ed4cda8dad9a95cbd67e661ac
julian-perge <julianperge@gmail.com>
2022-01-02 01:09:51 -0500

    Added logic for creating an encounter debug menu

--- Events/DebugPatches/Plugin.cs
+++ Events/DebugPatches/Plugin.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using BepInEx;
@@ -10,18 +12,108 @@ namespace DebugPatches
 	[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
 	public class Plugin : BaseUnityPlugin
 	{
-		public const string PluginGuid = "julianperge.inscryption.debugEnabling";
-		public const string PluginName = "EnableCertainDebugging";
-		private const string PluginVersion = "0.1.0";
+		public const string PluginGuid = "julianperge.inscryption.enableDebugTools";
+		public const string PluginName = "julianperge_EnableDebuggingTools";
+		private const string PluginVersion = "0.2.0";
 
 		internal static ManualLogSource Log;
+
+		private List<DiskCardGame.EncounterBlueprintData> resourceList;
+
+		private Dictionary<String, DiskCardGame.Opponent.Type> BossTypesByName = new()
+		{
+			{ "AnglerBossP1", DiskCardGame.Opponent.Type.AnglerBoss },
+			{ "AnglerBossP2", DiskCardGame.Opponent.Type.AnglerBoss },
+			{ "LeshyBossP1", DiskCardGame.Opponent.Type.LeshyBoss },
+			{ "ProspectorBossP1", DiskCardGame.Opponent.Type.ProspectorBoss },
+			{ "ProspectorBossP2", DiskCardGame.Opponent.Type.ProspectorBoss },
+			{ "TrapperTraderBossP1", DiskCardGame.Opponent.Type.TrapperTraderBoss },
+		};
 
 		private void Awake()
 		{
 			Log = base.Logger;
 
+			resourceList = UnityEngine.Resources
+				.LoadAll<DiskCardGame.EncounterBlueprintData>("Data/EncounterBlueprints/Part1").ToList();
+
 			var harmony = new Harmony(PluginGuid);
 			harmony.PatchAll();
+		}
+
+		private bool toggleEncounterMenu = true;
+
+		private void OnGUI()
+		{
+			string[] names = resourceList.Select(res => res.name).ToArray();
+
+			toggleEncounterMenu = UnityEngine.GUI.Toggle(
+				new UnityEngine.Rect(20, 280, 200, 20),
+				toggleEncounterMenu,
+				"Encounter Menu"
+			);
+
+			if (!toggleEncounterMenu) return;
+
+			int selectedButton = UnityEngine.GUI.SelectionGrid(
+				new UnityEngine.Rect(25, 300, 300, 300),
+				0,
+				names,
+				2
+			);
+
+			if (selectedButton > 0)
+			{
+				DiskCardGame.EncounterBlueprintData encounter = resourceList[selectedButton];
+				// the asset names have P1 or P2 at the end,
+				//	so we'll remove it so that we can correctly get a boss if a boss was selected
+				string scrubbedName = encounter.name.Replace("P1", "").Replace("P2", "");
+
+				DiskCardGame.CardBattleNodeData node = new DiskCardGame.CardBattleNodeData()
+				{
+					difficulty = DiskCardGame.RunState.Run.regionTier * 6 + 3 / 3 - 1, blueprint = resourceList[selectedButton]
+				};
+
+				if (Enum.TryParse(scrubbedName, true, out DiskCardGame.Opponent.Type bossType))
+				{
+					node = new DiskCardGame.BossBattleNodeData();
+					((DiskCardGame.BossBattleNodeData)node).specialBattleId =
+						DiskCardGame.BossBattleSequencer.GetSequencerIdForBoss(bossType);
+					((DiskCardGame.BossBattleNodeData)node).bossType = bossType;
+				}
+
+				DiskCardGame.Opponent opponent = Singleton<DiskCardGame.TurnManager>.Instance.Opponent;
+				if (opponent is not null && !Singleton<DiskCardGame.TurnManager>.Instance.GameIsOver())
+				{
+					Log.LogDebug($"Setting NumLives to zero");
+					opponent.NumLives = 1;
+					Log.LogDebug($"Game is not over, making opponent surrender");
+					opponent.SurrenderImmediate();
+					Log.LogDebug($"Playing LifeLostSequence for [{opponent.GetType()}]");
+					base.StartCoroutine(opponent.LifeLostSequence());
+				}
+
+				Log.LogDebug($"Transitioning to encounter [{resourceList[selectedButton].name}]");
+				CustomCoroutine.WaitOnConditionThenExecute(
+					() => Singleton<DiskCardGame.GameFlowManager>.Instance.CanTransitionToFirstPerson(), delegate
+					{
+						Log.LogDebug($"-> No longer in transitioning state, transitioning to new encounter");
+						Singleton<DiskCardGame.GameFlowManager>.Instance.TransitionToGameState(DiskCardGame.GameState.CardBattle,
+							node);
+					});
+			}
+
+			// EncounterData encounterData = EncounterBuilder.Build(bossBattleNodeData, true);
+			// encounterData.opponentTurnPlan =
+			// 	EncounterBuilder.BuildOpponentTurnPlan(encounterData.Blueprint, bossBattleNodeData.difficulty, false);
+			// encounterData.opponentType = bossBattleNodeData.bossType;
+			//
+			// EncounterData.StartCondition startCondition = new EncounterData.StartCondition
+			// {
+			// 	cardsInPlayerSlots = new[] { CardLoader.GetCardByName("Geck") },
+			// 	cardsInOpponentSlots = new[] { CardLoader.GetCardByName("Geck") }
+			// };
+			// encounterData.startConditions.Add(startCondition);
 		}
 	}
 
