34990b3c545338c26316c35181482320be2aa64b
julian-perge <julianperge@gmail.com>
2022-01-16 15:57:10 -0500

    Version 1.3.0

--- UI/IncreaseActOneCardSlots/Plugin.cs
+++ UI/IncreaseActOneCardSlots/Plugin.cs
@@ -5,7 +5,7 @@
 	{
 		public const string PluginGuid = "julianperge.inscryption.act1.increaseCardSlots";
 		public const string PluginName = "IncreaseActOneCardSlots";
-		private const string PluginVersion = "1.2.0";
+		private const string PluginVersion = "1.3.0";
 
 		internal static BepInEx.Logging.ManualLogSource Log;
 
