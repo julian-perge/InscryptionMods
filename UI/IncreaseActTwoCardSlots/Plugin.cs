cba63a5997fea2f50fe8d2a57cd3cbe871800bd1
julian-perge <julianperge@gmail.com>
2022-01-30 22:51:12 -0500

    Update to net6

--- UI/IncreaseActTwoCardSlots/Plugin.cs
+++ UI/IncreaseActTwoCardSlots/Plugin.cs
@@ -1,20 +1,19 @@
-﻿namespace IncreaseActTwoCardSlots
+﻿namespace IncreaseActTwoCardSlots;
+
+[BepInEx.BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+public class Plugin : BepInEx.BaseUnityPlugin
 {
-	[BepInEx.BepInPlugin(PluginGuid, PluginName, PluginVersion)]
-	public class Plugin : BepInEx.BaseUnityPlugin
+	public const string PluginGuid = "julianperge.inscryption.act2.increaseCardSlots";
+	public const string PluginName = "IncreaseActTwoCardSlots";
+	private const string PluginVersion = "0.1.0";
+
+	internal static BepInEx.Logging.ManualLogSource Log;
+
+	private void Awake()
 	{
-		public const string PluginGuid = "julianperge.inscryption.act2.increaseCardSlots";
-		public const string PluginName = "IncreaseActTwoCardSlots";
-		private const string PluginVersion = "0.1.0";
+		Log = base.Logger;
 
-		internal static BepInEx.Logging.ManualLogSource Log;
-
-		private void Awake()
-		{
-			Log = base.Logger;
-
-			var harmony = new HarmonyLib.Harmony(PluginGuid);
-			harmony.PatchAll();
-		}
+		var harmony = new HarmonyLib.Harmony(PluginGuid);
+		harmony.PatchAll();
 	}
 }
